03448758e8c7dd2ec2ed659b8b2746480ce69fa9
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-21 11:32:18 +0530

    About

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
         <div className="a-card bg"></div>
         <div className="a-card">
           <img
-            src="https://images.pexels.com/photos/3585047/pexels-photo-3585047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            src="https://www.freecodecamp.org/news/content/images/2022/01/ferenc-almasi-L8KQIPCODV8-unsplash.jpg"
             alt=""
             className="a-img"
           />
@@ -18,22 +18,64 @@ export const About = () => {
       <div className="a-right">
         <h1 className="a-title">About Me</h1>
         <p className="a-sub">
-          It is a long established fact that a reader will be distracted by the
-          readable content.
+          Crafting Immersive Web Experiences: A Front-End Developer's Journey in
+          React and JavaScript.
         </p>
         <p className="a-desc">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+          I am a seasoned front-end developer with a strong command of
+          JavaScript and a specialized focus on React. With a UX-centered
+          mindset, I tackle coding challenges innovatively, ensuring seamless
+          cross-browser experiences. My expertise lies in crafting responsive
+          designs, optimizing code, and fostering component reusability for
+          impactful web applications.
         </p>
         <div className="a-award">
-          <img src={Award} alt="" className="a-award-img" />
+          <a
+            href="https://www.udemy.com/certificate/UC-6645e96d-bb07-4156-b645-f9f28156c82c/"
+            target="_blank"
+          >
+            <img
+              src="https://udemy-certificate.s3.amazonaws.com/image/UC-6645e96d-bb07-4156-b645-f9f28156c82c.jpg"
+              alt=""
+              className="a-award-img"
+            />
+          </a>
           <div className="a-award-texts">
-            <h4 className="a-award-title">International Design Awards 2021</h4>
+            <h4 className="a-award-title">
+              JavaScript Proficiency Acknowledged: Udemy Certificate of
+              Completion
+            </h4>
             <p className="a-award-desc">
-              Nemo enim ipsam voluptatem quia voluptas sit aspernatur autodit
-              and fugit.
+              Enhancing my skills, I've earned a Certificate of Completion in
+              JavaScript from Udemy. This comprehensive course has fortified my
+              understanding of core concepts, enabling me to confidently apply
+              JavaScript's power to create dynamic and interactive web
+              solutions.
+            </p>
+          </div>
+        </div>
+        <div className="a-award">
+          <a
+            href="https://www.udemy.com/certificate/UC-b25d3264-31b9-4527-8c33-063199e513bc/"
+            target="_blank"
+          >
+            <img
+              src="https://udemy-certificate.s3.amazonaws.com/image/UC-b25d3264-31b9-4527-8c33-063199e513bc.jpg"
+              alt=""
+              className="a-award-img"
+            />
+          </a>
+          <div className="a-award-texts">
+            <h4 className="a-award-title">
+              Full Stack Web Developer Certification: Empowering Comprehensive
+              Web Solutions
+            </h4>
+            <p className="a-award-desc">
+              Earned a Full Stack Web Developer certification from Udemy,
+              showcasing my dedication to mastering front-end and back-end
+              technologies. Proficient in designing dynamic user interfaces,
+              optimizing databases, and implementing robust server-side
+              functions.
             </p>
           </div>
         </div>
